d29e252a8a6ccb5015fe2275988ddb7e8260784b
Wolfgang De Salvador <118554802+wolfgang-desalvador@users.noreply.github.com>
2023-10-29 00:32:28 +0200

    Update install_intel_libs.sh

--- rocky/common/install_intel_libs.sh
+++ rocky/common/install_intel_libs.sh
@@ -7,6 +7,11 @@ case ${DISTRIBUTION} in
         CHECKSUM="cc28c94cab23c185520b93c5a04f3979d8da6b4c90cee8c0681dd89819d76167";
         IDENTIFIER="IRC_NAS";
         ;;
+    "rocky8.8") INTEL_MKL_VERSION="2023.1.0.46342";
+        RELEASE_VERSION="cd17b7fe-500e-4305-a89b-bd5b42bfd9f8";
+        CHECKSUM="cc28c94cab23c185520b93c5a04f3979d8da6b4c90cee8c0681dd89819d76167";
+        IDENTIFIER="IRC_NAS";
+        ;;
     *) ;;
 esac
 

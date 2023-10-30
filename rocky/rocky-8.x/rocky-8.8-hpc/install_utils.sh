ab7d0849ebe276b2866a83504d4bb37c02a551da
Wolfgang De Salvador <118554802+wolfgang-desalvador@users.noreply.github.com>
2023-10-30 17:06:49 +0100

    Update install_utils.sh

--- rocky/rocky-8.x/rocky-8.8-hpc/install_utils.sh
+++ rocky/rocky-8.x/rocky-8.8-hpc/install_utils.sh
@@ -10,7 +10,7 @@ cp ./microsoft-prod.repo /etc/yum.repos.d/
 yum repolist
 
 # Install Kernel dependencies
-KERNEL=( $(rpm -q kernel | sed 's/kernel\-//g' | tail -n 1) )
+KERNEL=( $(rpm -q kernel | sed 's/kernel\-//g' | head -n 1) )
 yum install -y https://dl.rockylinux.org/pub/rocky/8.8/BaseOS/x86_64/os/Packages/k/kernel-devel-${KERNEL}.rpm \
     https://dl.rockylinux.org/pub/rocky/8.8/BaseOS/x86_64/os/Packages/k/kernel-headers-${KERNEL}.rpm \
     https://dl.rockylinux.org/pub/rocky/8.8/BaseOS/x86_64/os/Packages/k/kernel-modules-extra-${KERNEL}.rpm
